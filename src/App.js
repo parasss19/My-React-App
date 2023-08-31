70505d1b2d613cb4683d90649cb6a72773da67ab
Paras <mehtaparas1901@gmail.com>
2023-08-31 22:18:29 +0530

    modified readme

--- src/App.js
+++ src/App.js
@@ -27,13 +27,11 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
 
-      {/* expenses ka data yha h but humko ExpenseItem component m use krna h so we use prop */}
       <ExpenseItem title = {expenses[0].title} amount = {expenses[0].amount} date = {expenses[0].date} />
       <ExpenseItem title = {expenses[1].title} amount = {expenses[1].amount} date = {expenses[1].date} />
       <ExpenseItem title = {expenses[2].title} amount = {expenses[2].amount} date = {expenses[2].date} />
-      <ExpenseItem title = {expenses[3].title} amount = {expenses[3].amount} date = {expenses[3].date} />
+      <ExpenseItem title = {expenses[3].title} amount = {expenses[3].amount} date = {expenses[3].date} /> 
     </div>
   );
 }
