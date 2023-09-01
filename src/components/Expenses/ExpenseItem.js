41acc99564b2167d0870a0956bff72b1f8afd9c6
Paras <mehtaparas1901@gmail.com>
2023-09-01 23:33:33 +0530

    Completed Section 3

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,9 +1,13 @@
+import React from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
+import Card from '../UI/Card'
 
 function ExpenseItem(props) {
     return (
-        <div className='expense-item'>
+        //yha now hum div ki jagah apna component Card as wrapper use krenge
+        <Card className='expense-item'>
 
             {/* we took date as new component so app.js se jo data lekar aye the(date,title,amount)
              usme se date ko further pass krna hoga as usko new component m le gye h */}
@@ -13,7 +17,7 @@ function ExpenseItem(props) {
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-        </div>
+        </Card>
     );
 }
 
