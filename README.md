c9465d7bcf9615f9b945bff9d3aa447d3b35a767
Paras <mehtaparas1901@gmail.com>
2023-09-01 21:42:56 +0530

    Added Expenses component

--- README.md
+++ README.md
@@ -83,7 +83,6 @@ function App() {
             </div>
         </div>
         }
-
          ```
 
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,14 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
 function ExpenseItem(props) {
-    const month = props.date.toLocaleString('en-US' ,{month: 'long'})
-    const Day = props.date.toLocaleString('en-US', {day: '2-digit'})
-    const Year = props.date.getFullYear()
-
     return (
         <div className='expense-item'>
-            <div>{month}</div>
-            <div>{Day}</div>
-            <div>{Year}</div>
+
+            {/* we took date as new component so app.js se jo data lekar aye the(date,title,amount)
+             usme se date ko further pass krna hoga as usko new component m le gye h */}
+            <ExpenseDate date={props.date} />
+
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
@@ -19,3 +18,5 @@ function ExpenseItem(props) {
 }
 
 export default ExpenseItem;
+
+
